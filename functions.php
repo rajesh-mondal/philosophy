18a4fc1ba8169a7b8d3075f4fafeb507a6dbacc0
Rajesh Mondal <rajesh01749@gmail.com>
2021-03-16 23:45:21 +0600

    header navigation menu

--- functions.php
+++ functions.php
@@ -12,6 +12,8 @@ function philosophy_theme_setup() {
 	add_theme_support( 'html5', array( 'search-form', 'comment-list' ) );
 	add_theme_support( "post-formats", array( "image", "gallery", "quote", "audio", "video", "link" ) );
 	add_editor_style( "/assets/css/editor-style.css" );
+
+	register_nav_menu("topmenu",__("Top Menu","philosophy"));
 }
 
 add_action( "after_setup_theme", "philosophy_theme_setup" );
